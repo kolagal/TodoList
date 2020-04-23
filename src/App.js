cd90778ed64ee4302c3a2a5374803ef01c2ce201
Vanisha Srinidhi <vanisha.sk94@gmail.com>
2020-04-23 15:46:38 +0530

    TodoList

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  
+  render(){
+    return(
+  <div className = "App">
+    <header>
+    <form id="to-do-form" onSubmit={this.addItem}>
+      <input type = "text" placeholder="Enter Text"
+      value={this.state.currentItem.text}
+      onChange={this.handleInput}/>
+      <button type = "submit">Add</button>
+    </form>
+  </header>
+  </div>
+    );
+  }
 }
 
 export default App;
